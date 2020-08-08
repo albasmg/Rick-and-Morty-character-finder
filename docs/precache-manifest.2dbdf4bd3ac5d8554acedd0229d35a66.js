6f2eac78e8392950254a3e5fb6b57fb98a83b4fe
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-08-08 17:22:49 +0200

    add docs folder

---
+++ docs/precache-manifest.2dbdf4bd3ac5d8554acedd0229d35a66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b57eff31909763cc74bf9db7e98276",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a4a76dceea3a069bdda",
+    "url": "./static/css/main.f0693867.chunk.css"
+  },
+  {
+    "revision": "c1f01ba35bdcba0365b4",
+    "url": "./static/js/2.dcc53d95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dcc53d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4a76dceea3a069bdda",
+    "url": "./static/js/main.b275c763.chunk.js"
+  },
+  {
+    "revision": "13eb38f0fe3fe2d35a0b",
+    "url": "./static/js/runtime-main.00789ca4.js"
+  },
+  {
+    "revision": "a33cefa53533477270440d45f4b15293",
+    "url": "./static/media/morty.a33cefa5.png"
+  },
+  {
+    "revision": "7ff2f09f867a6af29340f1922e54bf69",
+    "url": "./static/media/rick.7ff2f09f.png"
+  },
+  {
+    "revision": "e743517e95664a6765911d46708f3346",
+    "url": "./static/media/rickandmorty-logo.e743517e.png"
+  }
+]);
