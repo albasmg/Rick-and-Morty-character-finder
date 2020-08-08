11b860dbf9cdd6ef9f5a3e142e3e73253acef934
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-08-08 17:29:33 +0200

    add form prevent submit

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 const Filters = ({ onSearchChange }) => {
   const handleInputChange = (ev) => onSearchChange(ev.target.value);
 
+  const handleInputSubmit = (ev) => ev.preventDefault();
+
   return (
     <div>
-      <form className="filter">
+      <form className="filter" onSubmit={handleInputSubmit}>
         <input
           type="text"
           onChange={handleInputChange}
